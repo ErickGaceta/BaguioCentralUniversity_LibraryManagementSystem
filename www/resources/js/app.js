396588ff704c2147abd77173d75bc78e9fb6508c
ErickGaceta <gacetaerick124@gmail.com>
2026-02-28 19:49:08 +0800

    uhh

--- www/resources/js/app.js
+++ www/resources/js/app.js
@@ -1,5 +1,7 @@
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import Clipboard from '@ryangjchandler/alpine-clipboard'
+import MontserratWoff from '../fonts/Montserrat-VariableFont_wght.ttf';
+import MontserratItalicWoff from '../fonts/Montserrat-Italic-VariableFont_wght.ttf';
 
 Alpine.plugin(Clipboard)
 
--- www/resources/views/partial/footer.blade.php
+++ www/resources/views/partial/footer.blade.php
@@ -1,6 +1,4 @@
     @fluxScripts
-    @livewireScripts
-    @livewireScriptConfig
 
     </body>
 
